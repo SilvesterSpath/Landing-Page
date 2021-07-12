edbf6a4ca6a1b789f77cd71d110b0e3c93691363
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-07-12 06:15:19 +0200

    modal

--- script.js
+++ script.js
@@ -2,18 +2,24 @@ const toggle = document.getElementById('toggle');
 const navbar = document.querySelector('nav');
 const open = document.getElementById('open');
 const close = document.getElementById('close');
-const modal = document.getElementById('modal');
+const modal = document.getElementById('modal-con');
 
-console.log(navbar);
-
+// Toggle navbar
 toggle.addEventListener('click', () => {
   document.body.classList.toggle('show-nav');
 });
 
+// Show modal
 open.addEventListener('click', () => {
-  modal.style.display = 'block';
+  modal.classList.add('show-modal');
 });
 
+// Hide modal
 close.addEventListener('click', () => {
-  modal.style.display = 'none';
+  modal.classList.remove('show-modal');
 });
+
+// Hide modal on outside click
+window.addEventListener('click', (e) =>
+  e.target == modal ? modal.classList.remove('show-modal') : false
+);
